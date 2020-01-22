285e43d0b7e22d874fa460a3cbc4ac70909b0ec2
H-Lala <hajiyeva.lalah@gmail.com>
2020-01-22 11:08:41 +0400

    -- sonarqube

---
+++ src/main/java/LoginServlet.java
@@ -0,0 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+         String name = req.getParameter("name");
+         String password = req.getParameter("password");
+        PrintWriter writer = resp.getWriter();
+            if(name.equalsIgnoreCase("user") && password.equalsIgnoreCase("111")) {
+                String htmlRespone = "<html>";
+                htmlRespone += "<h2>Your username is: " + name + "<br/>";
+                htmlRespone += "Your password is: " + password + "</h2>";
+                htmlRespone += "</html>";
+
+                // return response
+                writer.println(htmlRespone);
+                writer.close();
+            }
+    }
+}
